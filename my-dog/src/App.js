d763b337090a7b5dfde457363878f1928501df60
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-09-17 22:13:29 +0200

    add excercise 3

--- my-dog/src/App.js
+++ my-dog/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
+import NavBar from './components/NavBar';
+import PostList from './components/PostList';
+import Profile from './components/Profile';
+import SearchBar from './components/SearchBar'; 
+import Login from './components/Login'; 
+import axios from 'axios';
 
-function App() {
+function PhoneFrame({ children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="phone-frame">
+      <div className="content">{children}</div>
     </div>
   );
 }
 
-export default App;
+function App() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (value) => {
+    setSearchValue(value);
+  };
+
+  const [currentView, setCurrentView] = useState('postList');
+
+  const handleLogoClick = () => {
+    setCurrentView('postList');
+  };
+
+  const handleProfileClick = () => {
+    setCurrentView('profile');
+  };
+  
+  const [authenticated, setAuthenticated] = useState(false);
+
+  return (
+    <PhoneFrame>
+      <div className="container">
+        <NavBar
+          onLogoClick={handleLogoClick}
+          onProfileClick={handleProfileClick}
+        />
+        <SearchBar value={searchValue} onSearch={handleSearch} />
+        {authenticated ? (
+          currentView === 'postList' ? (
+            <PostList searchValue={searchValue} />
+          ) : (
+            <Profile />
+          )
+        ) : (
+          <Login onLogin={() => setAuthenticated(true)} />
+        )}
+      </div>
+    </PhoneFrame>
+  );
+        }
+  export default App;  
