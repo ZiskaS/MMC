151b34a818f70a2c97f72cf5f7669e4f6cf45f38
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-09-24 21:39:42 +0200

    added correct routes to navBar

--- my-dog-with-routes/src/components/navBar.jsx
+++ my-dog-with-routes/src/components/navBar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 function NavBar({ onLogoClick, onProfileClick }) {
   const location = useLocation();
 
   // Check if the current route is not '/login' and render the profile picture accordingly
-  const renderProfilePicture = location.pathname !== '/login' && (
-    <div onClick={onProfileClick}>
-      <i className="fa fa-user-circle"></i>
-    </div>
-  );
+  const renderProfilePicture =
+    location.pathname !== '/login' ? (
+      <div className="profile-icon" onClick={onProfileClick}>
+        <Link to="/profile">
+          <i className="fa fa-user-circle"></i>
+        </Link>
+      </div>
+    ) : null;
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container d-flex justify-content-between align-items-center">
-        <div className="navbar-brand" onClick={onLogoClick}>
-          I <i className="fa fa-heart"></i> My Dog
+      <div className="container-fluid p-2">
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <Link to="/" className="navbar-brand" onClick={onLogoClick}>
+            I <i className="fa fa-heart"></i> My Dog
+          </Link>
+          {renderProfilePicture}
         </div>
-        {renderProfilePicture}
       </div>
     </nav>
   );
 }
 
 export default NavBar;
-
